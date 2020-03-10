bdd89f9c0261e7bc707713629c13bd322ac72d0c
Tal Hayut <talhayut.dev@gmail.com>
2020-03-10 17:32:01 +0200

    ncc going live!

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # ncc action 
 github action to compile your github action and push to a destination branch!
+
+# Motivation
+* work on other devices which do not have a terminal
+* code directly on `github` - if that's a thing
+* `github hackathon` :)
+
+# Usage    
+```yaml
+jobs:
+  build:
+    runs-on: ubuntu-latest
+    steps:
+    - uses: actions/checkout@master
+    - uses: tool3/ncc@v1
+      with:
+        src: 'index.js'
+```
+
+# Options
+### `github_token`   
+   **required**   
+   Github access token
+### `src`   
+  **required**   
+  default: `index.js`
+### `branch`   
+  destination branch   
+  default: `master`
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const core = require('@actions/core');
 const { exec } = require('@actions/exec');
+const github = require('@actions/github');
+const path = require('path');
 const fs = require('fs');
 const util = require('util');
 const mkdir = util.promisify(fs.mkdir);
@@ -7,16 +9,35 @@ const write = util.promisify(fs.writeFile);
 
 async function run() {
   try {
-    const codeDirectory = core.getInput('dir');
+    const { pusher: { email, name } } = github.context.payload;
+    const inputBranch = core.getInput('branch');
+    const codeDirectory = core.getInput('src');
+    const resolvedCodeDirectory = path.join(__dirname, codeDirectory);
+    
+    await exec('git', ['config', '--local', 'user.name', name]);
+    await exec('git', ['config', '--local', 'user.email', email]);
 
-    require('@zeit/ncc')('./index.js', {
+    // compile code
+    require('@zeit/ncc')(resolvedCodeDirectory, {
       cache: false,
       sourceMapBasePrefix: '../', 
-    }).then(async ({ code, map, assets }) => {
-      core.info(code);
-      await mkdir('dist');
-      await write(`dist/index.js`, code);
+    }).then(async (everything) => {
+      const { code, assets } = everything;
+      // create dist folder
+      await mkdir('dist', {recursive: true});
+      
+      // create assets
+      Object.keys(assets).map(async asset => {
+        await write(`dist/${asset}`, assets[asset].source);
+      });
+
+      // write final code asset
+      const fileName = Object.keys(assets)[0].split('.')[0];
+      await write(`dist/${fileName}.js`, code);
     });
+
+    // push dist
+    await exec('git', ['push', 'origin', `HEAD:${inputBranch}`])
 
   } catch (error) {
     core.setFailed(`Failed to publish ${error.message}`);
