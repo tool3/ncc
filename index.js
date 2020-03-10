75a8875a3510e4a4a3b9d92f34f9d7808fdbcf5b
Tal Hayut <talhayut.dev@gmail.com>
2020-03-10 07:41:47 +0200

    initial ncc commit

--- index.js
+++ index.js
@@ -1,58 +1,23 @@
 const core = require('@actions/core');
 const { exec } = require('@actions/exec');
-const github = require('@actions/github');
-const { Toolkit } = require('actions-toolkit');
 
-const STRATEGIES = [
-  '#patch',
-  '#minor',
-  '#major'
-];
+async function run() {
+  try {
+    const code = core.getInput('dir');
 
-Toolkit.run(async tools => {
-  {
-    try {
-      // get context
-      const { pusher: { email, name }, head_commit: { message } } = github.context.payload;
+    require('@zeit/ncc')(code, {
+      cache: false,
+      externals: ["externalpackage"],
+      sourceMapBasePrefix: '../', 
+    }).then(({ code, map, assets }) => {
+      core.info(code);
+      console.log(map);
+      console.log(assets);
+    });
 
-      // get input credentials
-      const inputUser = core.getInput('user');
-      const inputEmail = core.getInput('email');
-      const inputBranch = core.getInput('branch');
-      const unrelated = core.getInput('unrelated');
-
-      const userName = inputUser || name;
-      const userEmail = inputEmail || email;
-
-      const defaultStrategy = STRATEGIES.filter(strat => message.includes(strat))[0] || STRATEGIES[0];
-      const strategy = defaultStrategy.replace('#', '');
-      const commitMessage = message.replace(defaultStrategy, '');
-
-      tools.log(`Latest commit message: ${commitMessage}`);
-      tools.log(`Running with ${userName} ${userEmail} and bumping strategy ${strategy}`);
-      tools.log(`Branch is ${inputBranch}`);
-
-
-      // git login and pull
-      const pullArgs = ['pull', 'origin', inputBranch, '--tags'];
-      if (unrelated) {
-        pullArgs.push('--allow-unrelated-histories');
-      }
-
-      await exec('git', ['config', '--local', 'user.name', userName]);
-      await exec('git', ['config', '--local', 'user.email', userEmail]);
-      await exec('git', pullArgs);
-
-      // version by strategy
-      await exec('npm', ['version', strategy, '--no-commit-hooks', '-m', `${commitMessage} %s`]);
-
-      // push new version and tag
-      await exec('git', ['push', 'origin', `HEAD:${inputBranch}`, '--tags'])
-
-    }
-    catch (error) {
-      core.setFailed(error.message);
-
-    }
+  } catch (error) {
+    core.setFailed(`Failed to publish ${error.message}`);
   }
-});
+}
+
+run();
