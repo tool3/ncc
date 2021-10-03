536f2c3579ea62aa6db465d1e97c253fdaf91ddb
sidey79 <7968127+sidey79@users.noreply.github.com>
2021-10-03 17:02:47 +0200

    Update index.js
    Remove git commit and push

--- index.js
+++ index.js
@@ -4,12 +4,7 @@ const path = require('path');
 
 async function run() {
   try {
-    await exec('git', ['config', '--local', 'user.name', 'GitHub Action']);
-    await exec('git', ['config', '--local', 'user.email', 'action@github.com']);
-
     // get input
-    const inputBranch = core.getInput('branch');
-    const commitMsg = core.getInput('commit_msg');
     const nccArgs = core.getInput('ncc_args');
     const allowUnrelated = core.getInput('allow_unrelated');
     const src = path.resolve(path.join(process.cwd(), core.getInput('src')));
@@ -34,15 +29,7 @@ async function run() {
 
     core.endGroup(`Compiling ${src}`);
 
-    core.startGroup('Pushing dist');
-
-    // push dist
-    await exec('git', ['add', 'dist/index.js']);
-    await exec('git', ['commit', '-a', '-m', commitMsg]);
-    await exec('git', ['push', 'origin', `HEAD:${inputBranch}`]);
-
-    core.endGroup('Pushing dist');
-    core.info('Compiled and pushed successfully 📦 🎉 ');
+    core.info('Compiled successfully 📦 🎉 ');
   } catch (error) {
     core.setFailed(`ncc failed! ${error.message}`);
   }
